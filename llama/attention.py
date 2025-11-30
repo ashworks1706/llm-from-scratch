48f2e97cb2272c960f72e8c52118305e824848e2
ashworks1706 <asriv132@asu.edu>
2025-11-29 17:21:57 -0700

    datasets]

--- llama/attention.py
+++ llama/attention.py
@@ -1,1 +1,9 @@
 
+from ..utils.config import Config
+
+class Llama:
+    def __init__(self, config):
+        self.config = Config
+     
+
+
--- llama/model.py
+++ llama/model.py
@@ -6,24 +6,4 @@ class Llama:
         self.config = Config(**config)
         self.logger = logger
 
-    def generate(self, prompt):
-        # get prompt and pass it through the model layers
-        result={};
-
-        return result
-    def train(self, data):
-        # Training logic here
-        pass
-
-    def evaluate(self, data):
-        # Evaluation logic here
-        pass
-    
-    def save_model(self, path):
-        # Logic to save the model to the specified path
-        pass
-
-    def get_config(self):
-        return self.config
-
-    
+   

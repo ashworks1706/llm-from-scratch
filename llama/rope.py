18053c39f9300873ffff44e1ce7b852d0343c08b
ashworks1706 <asriv132@asu.edu>
2025-11-29 17:50:14 -0700

    llama 3 from scratch

--- llama/rope.py
+++ llama/rope.py
@@ -1,2 +1,23 @@
 
+# Llama 3 uses Rotary Positional Embeddings instead of positional Embeddings
+# GPT 2 used to use absolute positional embeddings which basically meant that we use sinsonidal
+# and cosine embeddings for absolute positions of the tokens, which breaks if we try to 
+# generate text longer than it's trained on
+#
+# For some reason, putting tokens as different arrows instead of different positions, we
+# assign them different angles, we basically compare the angle difference kind of like
+# hands of clock, it results in better understanding
+#
 
+import torch
+
+def precompute_freqs_cis(dim,end,theta=500000.0):
+    """
+    Precomputes the angels we will use to rotate the vectors.
+
+    dim: dimension of the head (embedding size//num_heads) -> RoPE is applied to pairs 
+    of numbers, so we only need freq for dim/2.
+    end: the max seq length (context window)
+    theta: base frequency to handle very long context
+    what is frequency in this scenario? frequency is basically the speed of rotation for each dimension
+    """
--- utils/config.py
+++ utils/config.py
@@ -2,7 +2,7 @@
 class Config:
     def __init__(self, model_name, version, max_sequence_length, embedding_size,
                  num_attention_heads, num_layers, dropout_rate, learning_rate,
-                 batch_size, num_epochs, vocab_size, tokenizer_type):
+                 batch_size, num_epochs, vocab_size, tokenizer_type, num_kv_heads, rms_norm_eps, rope_theta):
         self.model_name = model_name
         self.version = version
         self.max_sequence_length = max_sequence_length
@@ -15,3 +15,6 @@ class Config:
         self.num_epochs = num_epochs
         self.vocab_size = vocab_size
         self.tokenizer_type = tokenizer_type
+        self.num_kv_heads = num_kv_heads
+        self.rms_norm_eps = rms_norm_eps
+        self.rope_theta = rope_theta

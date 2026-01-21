c437eb06e236ada61e9f5a1585ad754d7affc8d5
ashworks1706 <asriv132@asu.edu>
2026-01-21 15:00:17 -0700

    distillation

--- distilation/distill_with_lora.py
+++ distilation/distill_with_lora.py
@@ -1,1 +1,6 @@
 # distillation with LoRA adapters on student
+# we can performr distillation with LoRA obviosuly in this, where 
+# instead of trainign all studnet weeights, only train a few lora adapters 
+# so this turns in as even faster training (few parameters)
+# less memory (only adapter gradients)
+# can swap adapters easily (multiple students from one base )

cb4bc1c7bb7fc9641a6576e71ea5538aab659745
ashworks1706 <asriv132@asu.edu>
2026-01-21 15:14:20 -0700

    distilation

--- distilation/distill_with_lora.py
+++ distilation/distill_with_lora.py
@@ -4,3 +4,21 @@
 # so this turns in as even faster training (few parameters)
 # less memory (only adapter gradients)
 # can swap adapters easily (multiple students from one base )
+
+
+# we use inject lora from our previous files 
+
+
+from lora.inject_lora import inject_lora_to_model
+from distill_trainer import DistillationTrainer
+
+def create_lora_student(base_student_model, lora_config):
+    # inject LoRA to student model 
+    # freeze base weights 
+    # only keep LoRA adapeters trainable 
+    pass 
+
+
+def train_distillation_with_lora(teacher, student, conifg, dataset):
+    # create LoRA student
+    # use regular distillation trainer p

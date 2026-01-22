e3bd2c1e449f3112e10550095375996d85dd42b5
ashworks1706 <asriv132@asu.edu>
2026-01-22 07:27:13 -0700

    torch

--- neural_network/Pytorch/04_linear_layer.py
+++ neural_network/Pytorch/04_linear_layer.py
@@ -8,3 +8,43 @@
 # - understanding parameter shapes
 # - connection to matrix multiplication
 # - bias term (optional but important)
+
+class MyLinear(nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super().__init__()
+
+        # weight matrix (outfeat, infeat)
+        # we use this shape so we can do x @ weight.T
+        self.weight = nn.Parameter(torch.randn(out_features, in_Features))
+        # bias is (outfeat, )
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(out_features))
+        else:
+            self.bias = None
+
+
+        def forward(self, x ):
+            # x : batch, infeatr
+            # weight : outfeat, infeat
+            # weight.T : infeat, outfeat
+            
+            # matrix multiply 
+            output = x @ self.weight.T # batch,infeat @ infeat,outfeat
+            # why tanspose? PyTorch stores weights as (out_features, in_features)
+            # But we want: (batch, in_features) @ (in_features, out_features)           if self.bias is not None:
+            output + output + self.bias
+            return output
+
+
+my_linear = MyLinear(in_features=4, out_features=2)
+x = torch.randn(3,4) # batch pf 3 each w 4 features
+output = my_linear(x)
+print(f"Input shape: {x.shape}")
+print(f"Weight shape: {my_linear.weight.shape}")
+print(f"Output shape: {output.shape}")
+print(f"\nOur Linear output:\n{output}")
+
+# Compare with PyTorch's nn.Linear
+pytorch_linear = nn.Linear(4, 2)
+pytorch_output = pytorch_linear(x)
+print(f"\nPyTorch Linear output:\n{pytorch_output}")

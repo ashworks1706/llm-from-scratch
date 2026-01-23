95c48993403d2483372d3c81acd7f5306aee9bc8
ashworks1706 <asriv132@asu.edu>
2026-01-22 22:53:55 -0700

    torch

--- neural_network/Pytorch/05_activation_functions.py
+++ neural_network/Pytorch/05_activation_functions.py
@@ -1,2 +1,16 @@
 # activation functions from scratch
 # non linearity is what makes neural networks powerful
+
+
+# so the problem with neural networks was that whtout activations, it was becoming increasingly hard to code it out like a big chain equations
+# so we added activation functions to add one linear layer operations as a stack 
+
+# activations break linearity so netwroks can learn complex pattersn 
+
+
+
+# ReLU -> Rectified Linear Unit 
+# ReLU(x) = max(0, x) = {
+#         x   if x > 0
+#         0   if x ≤ 0
+#     }

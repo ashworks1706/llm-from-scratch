966a9ca81edc0fe26aad71f3c392e2eaf46a8b96
ashworks1706 <asriv132@asu.edu>
2026-01-29 00:19:46 -0700

    cnn

--- cnn/03_complete_cnn.py
+++ cnn/03_complete_cnn.py
@@ -50,3 +50,46 @@ test_in = torch.randn(2, 1, 28, 28)
 test_out = model(test_in)
 print(f"\nTest: {test_in.shape} → {test_out.shape}")
 
+
+
+
+class CNN(nn.module):
+    def __init__(self):
+        self.conv1= nn.Conv2d(1, 32,3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3= nn.Conv2d(64, 128,3, padding=1)
+        self.bn3 = nn.BatchNorm3d(128)
+        self.pool = nn.MaxPool2d(2,2)
+        self.fc1 = nn.Linear(128 * 3 * 3, 256)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(256, 10)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        x= self.pool(F.relu(self.bn2(self.conv2)))
+        x = self.pool(F.relu(self.bn3(self.conv3)))
+        x = x.view(-1, 128 * 3*3)
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.fc2(x)
+        return x
+
+cnn = CNN()
+
+print(cnn)
+
+
+
+
+
+
+
+
+
+
+
+
+

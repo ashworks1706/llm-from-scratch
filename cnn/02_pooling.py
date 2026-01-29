1791b9a74b31709ee50d9b93436878a2fd1769ae
ashworks1706 <asriv132@asu.edu>
2026-01-28 21:40:13 -0700

    cnn

--- cnn/02_pooling.py
+++ cnn/02_pooling.py
@@ -1,4 +1,9 @@
-# pooling operations from scratch
-# downsampling to reduce spatial dimensions
+# so in normal convolution what happens is that feature maps get larger and larger with conv layers increasing, this results in 
+# too much computatio nand too many parameters, if we flatten for FC layer, its in millions, which also lets model to overfit eventually wihtout spatial awarenss
+# pooling solves this by downsampling while keeping important informaiton
+# there's two types of pooling called max pooling, average pooling and global average pooling 
 
 
+# max pooling takes max value in each local region, why max? becasuse if a feature is detected, keep it
+# exact position doesn't matter, just that it exists, strong activates = important features 
+# 

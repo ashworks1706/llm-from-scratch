d90cebeb2d6fe3b585331d2019cf0344bb74c13c
ashworks1706 <asriv132@asu.edu>
2026-01-28 19:19:41 -0700

    cnn

--- cnn/02_pooling.py
+++ cnn/02_pooling.py
@@ -1,10 +1,4 @@
 # pooling operations from scratch
 # downsampling to reduce spatial dimensions
 
-# topics to cover:
-# - max pooling vs average pooling
-# - why pooling (dimension reduction, translation invariance)
-# - pooling window size and stride
-# - implementing maxpool2d and avgpool2d
-# - global average pooling
-# - effect on gradient flow
+

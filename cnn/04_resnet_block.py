e012b9d3c4c68e18aec42f177faa4a308482ce02
ashworks1706 <asriv132@asu.edu>
2026-01-31 10:53:59 -0700

    cnn

--- cnn/04_resnet_block.py
+++ cnn/04_resnet_block.py
@@ -108,9 +108,18 @@ import torch.nn.funtional as F
 class BasicBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
         super().__init__()
-        self.conv1 = nn.Conv3d(in_channels, out_channels, 3, stride=stride, padding=1, bias=False)
+        self.conv1 = nn.conv2d(in_channels, out_channels, 3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, stride=1, padding=1, bias= False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        # shortcut for dimension mismatch 
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, 1, stride=stride, bias = False),
+                nn.BatchNorm2d(out_channels)
+            )
 
 
 

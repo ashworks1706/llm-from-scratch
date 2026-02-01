d8cb1b0633a456f877a1486746f46d55b05e1405
ashworks1706 <asriv132@asu.edu>
2026-01-31 23:55:01 -0700

    lstm

--- lstm/01_vanilla_rnn.py
+++ lstm/01_vanilla_rnn.py
@@ -1,10 +1,45 @@
-# vanilla rnn cell from scratch
-# the simplest recurrent architecture
+# the whole story of dealing with RNNs and LSTMs is that we want to use temporal data or sequential data
+# that is to deal with text, time series, audio etc where order MATTERSS
 
-# topics to cover:
-# - recurrent connections (hidden state fed back)
-# - processing sequences one step at a time
-# - hidden state as memory
-# - forward pass through time
-# - vanishing gradient problem
-# - why vanilla rnns struggle with long sequences
+
+# instead of processing input all at once, we process it sequentially with memory 
+#
+#
+# this is how RNN did it : 
+# that is, take current input, combine with previous hidden state (memory), output prediction, pass hidden state to next step 
+# we take same weights and we use it every step 
+# like tanh(W_hh * h{t} + Wxh * x{t})
+
+
+
+# but the problem with RNN is that gradient at step 1 needs to flow through all 100 steps 
+# there fore in early layers if W_hh eigen values < 1 gradient vanishes exponentially 
+# so it doesnt learn anything later, like context issues 
+#
+#
+
+
+
+
+
+
+
+# that is why we have LSTM 
+# instead of simple hidden state update i.e 
+# h_t = tanh(W * [h_{t-1}, x_t])
+# we use gates to control information flow :
+# What to forget from memory
+# what to add to memory 
+# what to output from meory 
+# so basically it has memory cells and traffic controllers 
+
+
+
+
+
+
+
+
+
+
+

7d1fffe1f472e3a9bddf5e67c694fb8c9ec0d4f7
ashworks1706 <asriv132@asu.edu>
2026-01-31 22:09:43 -0700

    cnn

--- cnn/05_image_classifier.py
+++ cnn/05_image_classifier.py
@@ -1,4 +1,11 @@
 # CIFAR-10 -> dataaset with animals more realisticar than mnist 
 
 
+# to augment the data we do lot of flipping, cropping, resizing, jittering, normalization 
+# we need train test split 
+# fixed learning rate suboptimal, smooth decay following cosine curve LR scheduling
 
+
+
+class CIFAR:
+    def __init__

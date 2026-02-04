2cb9f83c0e1e9b79d777a02d06e89f8a65fb65b0
ashworks1706 <asriv132@asu.edu>
2026-02-04 09:27:57 -0700

    vae

--- vae/02_vae.py
+++ vae/02_vae.py
@@ -8,10 +8,7 @@
 # in VAE, we go -> image of '3' -> encoder -> u = [0.5,0.2] mean, sigma = [0.1,0.1] std deviation 
 # then we just sample form BOTH mean and std 
 # so then we get cloud of points around mean '3'
-
-
-
-# in VAE, we also get overlapping distributiosn that gives us conttinious smooth space 
+# we also get overlapping distributiosn that gives us conttinious smooth space 
 
 
 # So the main architecture for autoencoder and VAE is :
@@ -21,13 +18,6 @@
 # x -> Encoder -> [mean, std] -> sample z ~ N(mean, std^2) -> Decoder -> x 
 # so we get single point in normal encoder but in encoder of VAE, we get mean and log variance (for numerical stability)
 
-
-# notice how we used sampling from both mean and std, now since we're generating something, we need to backpropagate 
-# and for that we need gradient, but sampling is random, so its not differentiable 
-# the solution to this is to move randomness outside parameters, like normal sampling from normal number,
-# and transform that deterministically by epsilon 
-# so we can technically then call epsilon the randomness (independent of mean and std)
-# so trasnform is deterministic thus, differentiable 
 
 
 
@@ -47,11 +37,42 @@
 
 
 
+class Encoder(nn.Module):
+    def __init__(self, input_dim=784, hidden_dim=512, latent_dim=32):
+        super().__init__()
+
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.bn1 = nn.BatchNorm2d(hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, latent_dim)
+
+    def forward(self, x):
+        # notice how we used sampling from both mean and std, now since we're generating something, we need to backpropagate 
+        # and for that we need gradient, but sampling is random, so its not differentiable 
+        # the solution to this is to move randomness outside parameters, like normal sampling from normal number,
+        # and transform that deterministically by epsilon 
+        # so we can technically then call epsilon the randomness (independent of mean and std)
+        # so trasnform is deterministic thus, differentiable 
+        pass 
+
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dim, hidden_dim, output_dim):
+        super().__init__()
+
+        self.fc1 = nn.Linear(latent_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.bn1 = nn.BatchNorm2d(hidden_dim)
+
+    def forward(self, x):
+        pass 
 
 
 
 
+class AutoEncoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim, output_dim):
+        self.encoder = Encoder(input_dim, hidden_dim, latent_dim)
+        self.decoder = Decoder(latent_dim, hidden_dim, output_dim)
 
-
-
-
+    def forward(self, x):
+        pass 

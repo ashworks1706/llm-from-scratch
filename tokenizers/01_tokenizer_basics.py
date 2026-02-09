7fabf1fb7bae6854adbec3b906cc94252c94f341
ashworks1706 <asriv132@asu.edu>
2026-02-09 16:25:44 -0700

    tokenizers

--- tokenizers/01_tokenizer_basics.py
+++ tokenizers/01_tokenizer_basics.py
@@ -1,15 +1,15 @@
-# fundamental concepts of tokenization
-# how and why we convert text to integers
+# tokenizers are like the key parts of preprocessing data for LLMs 
+# it has the power to affect model vocabulary size (how mayn unique "pieces" the model knows ), determine the sequence lenght, same text is differetn number of tokens depending on different schemes 
+# and how well it handles rare/new words in terms of performance 
 
-# topics to cover:
-# - what is a token (subword vs word vs character)
-# - vocabulary and vocab size tradeoffs
-# - encode() method (text to ids)
-# - decode() method (ids back to text)
-# - special tokens (CLS, SEP, PAD, UNK, BOS, EOS)
-# - why we need padding and attention masks
-# - handling unknown words
-# - reversibility (can you always decode perfectly?)
 
-# OBJECTIVE: understand why "unhappiness" might become ["un", "happiness"]
-# see tradeoff between vocabulary size and sequence length
+# historically, tokenizers handled things character level, splitting words in to each cars like array, word less and subword level, 
+# subword level is the modern day tokenizer since it balances small vocabulary size, reasonable ssequecne legnths and ability ot handle unkwone rate words by breaking them into known pieces 
+
+
+# tokenizers are TRAINED, such that they know a bit of fixed vocabulary for nouns, verbs, etc. 
+# we also add [PAD] tokens to fill up unknown or short form 'don"t' type words 
+# such as [BOS] which means begining of seq, [EOS] end of seq, [UNK] unkown words, [CLS] classification for bert token, [SEP] used by bert again to seperate sentences 
+
+
+

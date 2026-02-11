9e028a55fc0807c4a7d376f99190f3b1b889962f
ashworks1706 <asriv132@asu.edu>
2026-02-11 16:36:28 -0700

    einops

--- einops/01_rearrange_basics.py
+++ einops/01_rearrange_basics.py
@@ -23,3 +23,39 @@ print(x.shape)
 flattened = rearrange(x, 'b h w -> b (h w)')
 print(flattened.shape)
 
+
+transposed = rearrange(x,"b h w -> b w h")
+print("transpose : ",transposed.shape)
+
+
+
+# add channel dimension by splitting one dimension 
+reshaped = rearrange(x, 'b h (w c) -> b h w c', c=2)
+print(reshaped.shape)
+
+
+batch, seq_len, embed_dim = 2,4, 8
+
+num_heads = 2
+head_dim = embed_dim // num_heads
+
+x = torch.randn(batch, seq_len, embed_dim)
+print("standard embedding : ", x.shape)
+
+reshaped_x = x.reshape(batch, seq_len, num_heads, head_dim).permute(0,2,1,3)
+print("standarrd way: ", reshaped_x.shape)
+
+x_heads = rearrange(x, 'b n (h d) -> b h n d', h=num_heads)
+print(x_heads.shape)
+
+
+# getting x head sback 
+#
+x_combined = rearrange(x_heads, 'b h n d -> b n (h d)')
+print(x_combined.shape)
+
+
+images = torch.randn(2,4,4,3)
+
+
+# average pooling 

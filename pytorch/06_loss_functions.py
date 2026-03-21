78a713296dc9af5f2279706543731625f6211f77
ashworks1706 <asriv132@asu.edu>
2026-03-21 14:32:02 -0700

    added images

--- pytorch/06_loss_functions.py
+++ pytorch/06_loss_functions.py
@@ -91,7 +91,7 @@ print(f"Batch CE loss: {loss:.4f}")
 # Minimizing cross-entropy = maximizing the likelihood of the correct class = reducing negative log likelihood  
  
    
-       
+        
 # why not MSE for classification ? 
 # Example: 3-class problem
 logits = torch.tensor([[2.0, 1.0, 0.1]])  # Batch of 1

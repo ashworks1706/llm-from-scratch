81576dee8f7083ab70fe2ce33f641a22318694d7
ashworks1706 <asriv132@asu.edu>
2026-03-31 17:23:41 -0700

    stable diffusion

--- stablediffusion/unet.py
+++ stablediffusion/unet.py
@@ -1,1 +1,2 @@
-# unet is used for adding timestepped noise and denoise process for understanding image structure
+# unet is used for adding timestepped noise and denoise process for understanding image structure from
+# encoder latent state and clip embedding

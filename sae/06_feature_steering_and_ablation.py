5c3ea487abb783ecd92ada968530bcb5ded384a4
ashworks1706 <asriv132@asu.edu>
2026-04-13 00:29:54 -0700

    new docs

--- sae/06_feature_steering_and_ablation.py
+++ sae/06_feature_steering_and_ablation.py
@@ -1,3 +1,9 @@
 # now that we understand what SAEs are, what those features are looking and understanding how they are represented 
 # If "we directly manipulate latent feature i, does model behavior change in the predicted direction?"
-#
+
+
+
+# casual framing :
+# we know that z_i fires on pattern P 
+# so what if we force z_i down (ablation) or up (steering) and check if behavior X tied to P changes 
+# if yes then we can derive that z_i -> X concept is casually relevent

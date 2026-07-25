03ad485a69234e890beb32d213890fbeeefb0279
ashworks1706 <asriv132@asu.edu>
2026-07-25 22:58:50 +0530

    inference day 1

--- inference/fundamentals/01_inference_execution.rs
+++ inference/fundamentals/01_inference_execution.rs
@@ -12,12 +12,37 @@
 
 use std::time::Instant;
 
-use tokenizers::Tokenizer;
+use std::error::Error;
+use tokenizers::{Tokenizer}; 
 
-fn main() {
+type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
+
+fn main() -> Result<()> {
     // 1. tokenize prompt text into token ids
+
+    let tokenizer = Tokenizer::from_pretrained("bert-base-cased", None)?;
+    let encoding = tokenizer.encode("Hello world!", false)?;
+    println!("Token IDs: {:?}", encoding.get_ids());
+
+    // Token IDs: [8667, 1362, 106]
+
     // 2. run the decoder-only forward pass over the tokens
+
+    let decoding = tokenizer.decode(encoding.get_ids(), true)?;
+    println!("Decoded text: {}", decoding);
+
     // 3. select the next token from the output logits
+
+    let next_token = encoding.get_ids().last().unwrap();
+    println!("Next token ID: {}", next_token);
+
     // 4. append and repeat until a stop condition
+    
+
+    encoding.get_ids().iter().for_each(|token_id| {
+        println!("Token ID: {}", token_id);
+    });
+
     // 5. decode the generated token ids back into text
+    Ok(())
 }

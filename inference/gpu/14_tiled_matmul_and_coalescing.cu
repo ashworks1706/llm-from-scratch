79df34b3e3b1b5f8636fddd417acd86b918aada8
ashworks1706 <asriv132@asu.edu>
2026-08-15 22:46:09 +0530

    inference day 16

--- inference/gpu/14_tiled_matmul_and_coalescing.cu
+++ inference/gpu/14_tiled_matmul_and_coalescing.cu
@@ -37,9 +37,52 @@ __global__ void matmul_naive(int n, const int *A, const int *B, int *C) {
 // we can think about these tiles as like a sliding window, where we slide the tiles across the 
 // matrices A and B, and compute the product of the tiles, and accumulate the result into C.
 __global__ void tiled_matmul(int n, int *A, int *B, int *C){
+  
+  // 1. shared memory for tiles of A and B 
   __shared__ int tile_A[16][16];
   __shared__ int tile_B[16][16];
 
+  
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+
+  // global row and col of matrix C 
+  int row = blockIdx.y * blockDim.y + ty;
+  int col = blockIdx.x * blockDim.x + tx;
+
+  float sum = 0;
+
+  // loop over tiles of A and B 
+  int num_tiles = (n + 16 - 1) / 16; // 16-> tile size 
+  for(int t=0; t<num_tiles;t++){
+    // load 1 element per thread into shared memory 
+    if (row < n && t*16 + tx < n){
+      tile_A[ty][tx] = A[row * n + t * 16 + tx];
+    } else{
+      tile_A[ty][tx] = 0;
+    }
+
+    if (col < n && t*16 + ty < n){
+      tile_B[ty][tx] = B[(t * 16 + ty) * n + col];
+    } else{
+      tile_B[ty][tx] = 0;
+    }
+
+    __syncthreads(); // phase b : wait for all threads to load their elements into shared memory
+
+    // phase c : compute the product of the tiles and accumulate into sum 
+
+    for(int k=0; k<16;k++){
+      sum += tile_A[ty][k] * tile_B[k][tx];
+    }
+
+    __syncthreads(); // phase d : wait for all threads to finish computing their sum before loading the next tile 
+
+  }
+
+  if (row < n && col < n){
+    C[row * n + col] = sum;
+  }
 
 }
 
@@ -64,7 +107,8 @@ int main() {
   cudaMemcpy(d_A, h_A, n * n * sizeof(int), cudaMemcpyHostToDevice);
   cudaMemcpy(d_B, h_B, n * n * sizeof(int), cudaMemcpyHostToDevice);
 
-  matmul_naive<<<grid_dim, block_dim>>>(n, d_A, d_B, d_C);
+  // matmul_naive<<<grid_dim, block_dim>>>(n, d_A, d_B, d_C);
+  tiled_matmul<<<grid_dim, block_dim>>>(n, d_A, d_B, d_C);
 
   cudaMemcpy(h_C, d_C, n * n * sizeof(int), cudaMemcpyDeviceToHost);
   cudaDeviceSynchronize();

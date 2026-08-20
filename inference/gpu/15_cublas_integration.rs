43daddf533de07d9f90d1b7591b5853b3e1d7608
ashworks1706 <asriv132@asu.edu>
2026-08-20 15:36:49 +0530

    inference day 19

--- inference/gpu/15_cublas_integration.rs
+++ inference/gpu/15_cublas_integration.rs
@@ -13,6 +13,7 @@
 
 #[allow(unused_imports)]
 use {cudarc::cublas::CudaBlas, cudarc::driver::CudaContext};
+use {cudarc::cublas::Gemm};
 
 
 // let's prepare a normal linear layer that has an input and output layer, in normal perceptron,
@@ -26,6 +27,15 @@ struct LinearLayer{
 }
 
 fn main() -> anyhow::Result<()> {
+ 
+    // in transformers, there's lot of matri multiplications so in order to do it fastly we need to
+    // treat matrix xas column major rather than row major multiplication 
+    // such that, C = A x B is also C^T = A^T x B^T 
+    // since row majored A = col majored A's transpose 
+
+    // this is GEMM, general matrix multiply, that aims to solve 
+    // C = \alpha . op(A) . op(B) + \beta + C 
+    
     let ctx = CudaContext::new(0)?;
     
     let stream = ctx.default_stream(); 
@@ -36,16 +46,22 @@ fn main() -> anyhow::Result<()> {
 
     // CudaBlas { handle: 0x556cd7c70660, stream: CudaStream { cu_stream: 0x0, ctx: CudaContext { cu_device: 0, cu_ctx: 0x556cd7021700, ordinal: 0, has_async_alloc: true, is_primary: 
     // true, num_streams: 0, event_tracking: true, error_state: 0 } } }
-
-    // in transformers, there's lot of matri multiplications so in order to do it fastly we need to
-    // treat matrix xas column major rather than row major multiplication 
-    // such that, C = A x B is also C^T = A^T x B^T 
-    // since row majored A = col majored A's transpose 
-
-    // this is GEMM, general matrix multiply, that aims to solve 
-    // C = \alpha . op(A) . op(B) + \beta + C 
     
+    let n = 1024;
 
+    let h_a = vec![1i32; n * n];
+    let h_b = vec![2i32; n * n];
+     
+    // output  
+    let h_o = vec![0i32; n * n];
+     // device side pointers (input + output)
+    let d_a = stream.clone_htod(&h_a)?; 
+    let d_b = stream.clone_htod(&h_b)?;
+    // output 
+    let mut d_o = stream.clone_htod(&h_o)?;
+
+
+    blas.gemm(cfg, d_a, d_b, d_o);
 
     Ok(())
 }

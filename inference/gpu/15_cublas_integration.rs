ca1fea34b747c20391b21f49c746b88c214c6928
ashworks1706 <asriv132@asu.edu>
2026-08-20 15:31:23 +0530

    inference day 19

--- inference/gpu/15_cublas_integration.rs
+++ inference/gpu/15_cublas_integration.rs
@@ -14,6 +14,17 @@
 #[allow(unused_imports)]
 use {cudarc::cublas::CudaBlas, cudarc::driver::CudaContext};
 
+
+// let's prepare a normal linear layer that has an input and output layer, in normal perceptron,
+// we have weights and biases
+// for this we've got input X[M,K] with weight W[K,N] and output Y[M,N]
+
+struct LinearLayer{
+    _in : [4, 4096], // M=4 tokens, K=4096 hidden dim 
+    _w : [4096, 11008], // K=4096 hidden dim, N=11008 MLP projection
+    _o : [4, 11008], // output layer 
+}
+
 fn main() -> anyhow::Result<()> {
     let ctx = CudaContext::new(0)?;
     
@@ -21,9 +32,19 @@ fn main() -> anyhow::Result<()> {
 
     let blas = CudaBlas::new(stream.clone())?;
 
+    println!("{:?}", blas);
 
+    // CudaBlas { handle: 0x556cd7c70660, stream: CudaStream { cu_stream: 0x0, ctx: CudaContext { cu_device: 0, cu_ctx: 0x556cd7021700, ordinal: 0, has_async_alloc: true, is_primary: 
+    // true, num_streams: 0, event_tracking: true, error_state: 0 } } }
 
+    // in transformers, there's lot of matri multiplications so in order to do it fastly we need to
+    // treat matrix xas column major rather than row major multiplication 
+    // such that, C = A x B is also C^T = A^T x B^T 
+    // since row majored A = col majored A's transpose 
 
+    // this is GEMM, general matrix multiply, that aims to solve 
+    // C = \alpha . op(A) . op(B) + \beta + C 
+    
 
 
     Ok(())
